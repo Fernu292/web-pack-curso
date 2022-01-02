90ec5378363d6b8739f3be5b0083123fdb88cb71
Luis Fernando <fernu292@gmail.com>
2022-01-01 19:08:21 -0600

    Fix big in script

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 //Creando script para servidor, que pueda usar variables de entorno
-fs.writeFileSync('./env', `API=${process.env.API}\n`);
+fs.writeFileSync('./.env', `API=${process.env.API}\n`);
